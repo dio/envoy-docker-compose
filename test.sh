54c2c29b4cb42940f20ada10f72df507717a375a
Dhi Aurrahman <dio@tetrate.io>
2021-11-11 15:03:57 +0700

    Fix test script (#1)
    This patch fixes the test.sh, particularly on how we check the response.
    
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- test.sh
+++ test.sh
@@ -2,11 +2,12 @@
 
 captured=$(docker run --network container:frontend \
   appropriate/curl -s \
+  --write-out %{http_code} \
+  --output /dev/null \
   --retry 10 \
   --retry-connrefused \
-  -I \
-  http://localhost:10000 | grep 'HTTP/1.1 200 OK')
+  http://localhost:10000)
 
-if [ "$captured" != "HTTP/1.1 200 OK" ]; then
+if [ ${captured} -ne 200 ]; then
   exit 1
 fi
