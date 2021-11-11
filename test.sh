f059974a817487c5b30562b28a8cf1401e2aa234
Dhi Aurrahman <dio@rockybars.com>
2021-11-11 00:56:38 -0700

    Debug CI
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- test.sh
+++ test.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+docker run --network container:frontend \
+  appropriate/curl -s \
+  --retry 10 \
+  --retry-connrefused \
+  -I \
+  http://localhost:10000
+
 captured=$(docker run --network container:frontend \
   appropriate/curl -s \
   --retry 10 \
   --retry-connrefused \
   -I \
   http://localhost:10000 | grep 'HTTP/1.1 200 OK')
+
+echo ${captured}
 
 if [ "$captured" != "HTTP/1.1 200 OK" ]; then
   exit 1
